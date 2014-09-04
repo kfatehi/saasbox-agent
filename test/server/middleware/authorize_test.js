70a67112174e6faffc9f5a9d6e9c29263d38731b
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-04 05:13:06 -0700

    simplify tests

---
+++ test/server/middleware/authorize_test.js
@@ -0,0 +1,48 @@
+var expect = require('chai').expect
+  , mw = require('../../../src/server/middleware/authorize')
+  , secret = require('../../../src/secret')
+
+it("rejects incorrect token", function(done) {
+  var req = {
+    get: function(header) {
+      return headers[header]
+    }
+  }
+  var headers = {
+    'X-Auth-Token': 'wrong!!'
+  }
+  var res = {
+    status: function(code) {
+      expect(code).to.eq(401)
+      return { end: done }
+    }
+  }
+  mw(req, res);
+});
+
+it("rejects missing token", function(done) {
+  var req = {
+    get: function(header) {
+      return undefined
+    }
+  }
+  var res = {
+    status: function(code) {
+      expect(code).to.eq(401)
+      return { end: done }
+    }
+  }
+  mw(req, res);
+});
+
+it("accepts correct token", function(done) {
+  var req = {
+    get: function(header) {
+      return headers[header]
+    }
+  }
+  var headers = {
+    'X-Auth-Token': secret
+  }
+  mw(req, null, done);
+});
--- test/server/routes/drops_test.js
+++ test/server/routes/drops_test.js
@@ -1,73 +1,19 @@
-process.env.STORAGE_PATH = '/tmp/saasbox-agent-test'
-process.env.API_SECRET = 'secret'
-
 var expect = require('chai').expect
   , request = require('supertest')
   , app = require('../../../src/server/app').api.app
-  , target = require('../../../src/target')
+  , secret = require('../../../src/secret')
 
-describe("API v1 Routes", function() {
-  beforeEach(function() {
-    target.clearAll();
+describe("POST /api/v1/drops/:name", function () {
+  it("creates the proxy target and returns 204 no content", function(done) {
+    request(app)
+    .post('/api/v1/drops/foo')
+    .set('X-Auth-Token', secret)
+    .send('code()')
+    .expect(201)
+    .end(function(err, res) {
+      if (err) throw err;
+      expect(err).to.eq(null)
+      done()
+    })
   });
-
-  describe("DELETE /api/v1/proxies/:fqdn", function () {
-
-    beforeEach(function(done) {
-      target.set('bottom.middle.top', 'stuff', done)
-    });
-
-    describe("no token", function() {
-      it("does not delete and returns 401 unauthorized", function(done) {
-        request(app)
-        .delete('/api/v1/proxies/bottom.middle.top')
-        .send({ target: 'http://localhost:12345' })
-        .expect(401)
-        .end(function(err, res) {
-          if (err) throw err;
-          target.get('bottom.middle.top', function(err, reply) {
-            expect(err).to.eq(null)
-            expect(reply).to.eq('stuff')
-            done()
-          })
-        })
-      });
-    });
-
-    describe("wrong token", function() {
-      it("does not delete the target and returns 401 unauthorized", function(done) {
-        request(app)
-        .delete('/api/v1/proxies/bottom.middle.top')
-        .set('X-Auth-Token', 'password')
-        .send({ target: 'http://localhost:12345' })
-        .expect(401)
-        .end(function(err, res) {
-          if (err) throw err;
-          target.get('bottom.middle.top', function(err, reply) {
-            expect(err).to.eq(null)
-            expect(reply).to.eq('stuff')
-            done()
-          })
-        })
-      });
-    });
-
-    describe("valid token", function() {
-      it("deletes the proxy target and returns 204 no content", function(done) {
-        request(app)
-        .delete('/api/v1/proxies/bottom.middle.top')
-        .set('X-Auth-Token', 'secret')
-        .send({ target: 'http://localhost:12345' })
-        .expect(204)
-        .end(function(err, res) {
-          if (err) throw err;
-          target.get('bottom.middle.top', function(err, reply) {
-            expect(err).to.eq(null)
-            expect(reply).to.eq(null)
-            done()
-          })
-        })
-      });
-    });
-  })
 })
