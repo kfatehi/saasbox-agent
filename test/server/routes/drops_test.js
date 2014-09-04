b90607baa235cad463495a80dbf73822f39ddf23
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-04 05:33:20 -0700

    test perform route

--- test/server/routes/drops_test.js
+++ test/server/routes/drops_test.js
@@ -3,16 +3,48 @@ var expect = require('chai').expect
   , app = require('../../../src/server/app').api.app
   , secret = require('../../../src/secret')
 
+
 describe("POST /api/v1/drops/:name", function () {
-  it("creates the proxy target and returns 204 no content", function(done) {
+  var code = null;
+
+  beforeEach(function() {
+    code = "module.exports = "+function() {
+      return {
+        install: function(cb) {
+          cb(null, { hello: 'world' })
+        }
+      }
+    }.toString()
+  });
+
+  it("creates drop and returns 201 created", function(done) {
     request(app)
     .post('/api/v1/drops/foo')
     .set('X-Auth-Token', secret)
-    .send('code()')
+    .set('Content-Type', 'application/javascript')
+    .send(code)
     .expect(201)
     .end(function(err, res) {
       if (err) throw err;
       expect(err).to.eq(null)
+      done()
+    })
+  });
+})
+
+describe("POST /api/v1/drops/:name/:action", function () {
+  it("performs the action and returns the output", function(done) {
+    request(app)
+    .post('/api/v1/drops/foo/install')
+    .set('X-Auth-Token', secret)
+    .send({
+      namespace: 'myuser'
+    })
+    .expect(200)
+    .end(function(err, res) {
+      if (err) throw err;
+      expect(err).to.eq(null)
+      expect(res.body.hello).to.eq('world')
       done()
     })
   });
