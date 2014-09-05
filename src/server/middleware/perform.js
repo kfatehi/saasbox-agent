498951e453af6b251919fcb20e2db16004a5534a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-04 22:53:32 -0700

    handle docker connection error

--- src/server/middleware/perform.js
+++ src/server/middleware/perform.js
@@ -6,8 +6,14 @@ module.exports = function(req, res, next) {
   var performer = ydm.performer(req.params.name, req.body)
   if (performer.canPerform(req.params.action)) {
     performer.perform(req.params.action, function(err, out) {
-      if (err) res.status(500).end(err.message);
-      if (out) {
+      if (err) {
+        res.status(500)
+        if (err.syscall === 'connect') {
+          res.end('Cannot connect to Docker'); 
+        } else {
+          res.end(err.message)
+        }
+      } if (out) {
         res.status(200)
         if (_.isObject(out)) {
           res.json(out);
