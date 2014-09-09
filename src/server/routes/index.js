0f69ddd08e173f26e334c21504216d23295cf14a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-09-09 04:56:53 -0700

    add version check route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,4 +1,8 @@
+var version = require('../../../package').version;
 var router = require('express').Router()
+router.get('/version', function(req, res) {
+  res.json({ version: version });
+})
 require('./proxies')(router)
 require('./drops')(router)
 module.exports = router
