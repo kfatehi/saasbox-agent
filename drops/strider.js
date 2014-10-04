776ced81cb0e6f6cec26de282a5a5c43b2b9d18c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-03 22:39:51 -0700

    merge config into env

--- drops/strider.js
+++ drops/strider.js
@@ -1,20 +1,16 @@
+var _ = require('lodash')
+
 module.exports = function(scope, argv) {
   return {
     install: function (done) {
       scope.applyConfig({
         create: {
           Image: "quay.io/keyvanfatehi/strider:1.5.0",
-          Env: {
+          Env: _.assign({
             /* https://github.com/Strider-CD/strider#configuring */
-            SERVER_NAME: "https://"+argv.fqdn,
-            PLUGIN_GITHUB_APP_ID: argv.config.PLUGIN_GITHUB_APP_ID,
-            PLUGIN_GITHUB_SECRET: argv.config.PLUGIN_GITHUB_SECRET,
-            SMTP_HOST: argv.config.SMTP_HOST,
-            SMTP_USER: argv.config.SMTP_USER,
-            SMTP_PASS: argv.config.SMTP_PASS,
-            SMTP_FROM: argv.config.SMTP_FROM
+            SERVER_NAME: "https://"+argv.fqdn
             //SMTP_FROM: 'Strider-CD <no-reply@'+argv.fqdn+'>'
-          }
+          }, argv.config || {})
         },
         start: {
           Binds: scope.managedVolumes({
