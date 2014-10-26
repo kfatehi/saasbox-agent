7a1fc17e0f89081b16be56619dd8e48767072308
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-26 13:24:37 -0700

    add stats api

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,10 @@
-var version = require('../../../package').version;
-var router = require('express').Router()
+var version = require('../../../package').version
+  , router = module.exports = require('express').Router()
+  , sss = require('simple-stats-server')
+
+router.use('/stats', sss())
 router.get('/version', function(req, res) {
   res.json({ version: version });
 })
 require('./proxies')(router)
 require('./drops')(router)
-module.exports = router
