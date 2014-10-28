dbb9da7e44e4fab98b1285731a8cc32277913352
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-10-27 19:25:56 -0700

    improve common

--- src/common.js
+++ src/common.js
@@ -1,8 +1,11 @@
-module.exports = require('upstreamapp-common')({
+var common = require('upstreamapp-common')
+var config = {
   logger: {
     papertrail: {
       host: 'logs2.papertrailapp.com',
       port: 22069
     }
   }
-})
+}
+
+module.exports = process.env.NODE_ENV === 'production' ? common(config) : common()
